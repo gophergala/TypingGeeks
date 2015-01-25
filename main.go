874a7a3eb6d78e58cc3d912b6baf14e93705205b
parnurzeal <parnurzeal@gmail.com>
2015-01-26 02:11:36 +0900

    Refactored variable names and not put key process in another word go routine

--- main.go
+++ main.go
@@ -12,25 +12,28 @@ import (
 
 var mutex = &sync.Mutex{}
 
-var wordList = []string{"cat", "dog", "max", "delicious", "games", "ant", "min"}
+var wordList = []string{"cat", "card", "cog", "cam", "camfrog", "caddie", "cammy"}
+
+//var wordList = []string{"cat", "dog", "max", "delicious", "games", "ant", "min"}
 
 type Word struct {
-	r, c     int
-	str      string
-	velo     int
-	progress int
+	r, c         int
+	str          string
+	velo         int
+	progress     int
+	typedKeyChan chan rune
 }
 
 type TypingGeeks struct {
-	eventChan  chan termbox.Event
-	exitChan   chan string
-	wordMap    map[rune]Word
-	wordChan   chan Word
-	keyChan    chan rune
-	curWordKey rune
-	rowSize    int
-	colSize    int
-	fps        int // frame per sec, must be between 1-60, default is 25
+	eventChan    chan termbox.Event
+	exitChan     chan string
+	wordMap      map[rune]Word
+	wordChan     chan Word
+	typedKeyChan chan rune
+	curWordKey   rune
+	rowSize      int
+	colSize      int
+	fps          int // frame per sec, must be between 1-60, default is 25
 
 }
 
@@ -48,7 +51,7 @@ func (t *TypingGeeks) Initialise() {
 	t.exitChan = make(chan string)
 	t.wordMap = make(map[rune]Word)
 	t.wordChan = make(chan Word)
-	t.keyChan = make(chan rune)
+	t.typedKeyChan = make(chan rune)
 	// screen related
 	t.rowSize = 30
 	t.colSize = 60
@@ -63,8 +66,15 @@ func (t *TypingGeeks) WaitExit() {
 func (t *TypingGeeks) GoWordFeeder() {
 	counter := 0
 	for {
-		t.wordChan <- Word{0, rand.Intn(t.colSize), wordList[counter], 2, 0}
-		time.Sleep(800 * time.Millisecond)
+		t.wordChan <- Word{
+			r:            0,
+			c:            rand.Intn(t.colSize),
+			str:          wordList[counter],
+			velo:         1,
+			progress:     0,
+			typedKeyChan: make(chan rune),
+		}
+		time.Sleep(500 * time.Millisecond)
 		counter++
 		if counter >= len(wordList) {
 			counter = 0
@@ -88,14 +98,43 @@ func (t *TypingGeeks) GoMainProcessor() {
 					veloSleepTime := time.Duration(1000000/newWord.velo) * time.Microsecond
 					time.Sleep(veloSleepTime)
 					// due to issue #3117, we gotta assign value like this for map of struct for now.
-					tmp := t.wordMap[key]
-					tmp.r++
-					t.wordMap[key] = tmp
+					curWord, exist := t.wordMap[key]
+					if !exist {
+						return
+					}
+					curWord.r++
+					t.wordMap[key] = curWord
 					// delete word that goes out of windows in wordMap
 					// TODO: need to watch out of race condition for map, too. see -> https://blog.golang.org/go-maps-in-action
 					if t.wordMap[key].r > t.rowSize {
 						delete(t.wordMap, key)
 						return
+					}
+				}
+			}(key)
+			go func(key rune) {
+				for {
+					typedKey := <-t.wordMap[key].typedKeyChan
+					curWord, exist := t.wordMap[key]
+					if !exist {
+						return
+					}
+					for pos, char := range curWord.str {
+						if pos == curWord.progress {
+							if char == typedKey {
+								curWord.progress++
+								if curWord.progress >= len(curWord.str) {
+									// TODO: finish whole word, implement successful attempt effect
+									delete(t.wordMap, t.curWordKey)
+									t.curWordKey = 0
+									return
+								}
+								t.wordMap[t.curWordKey] = curWord
+								break
+							} else {
+								// TODO: wrong key, implement fail attempt effect
+							}
+						}
 					}
 				}
 			}(key)
@@ -125,15 +164,15 @@ func (t *TypingGeeks) GoRender() {
 func (t *TypingGeeks) GoKeyAnalyzer() {
 	for {
 		select {
-		case key := <-t.keyChan:
+		case typedKey := <-t.typedKeyChan:
 			if t.curWordKey != 0 {
-
+				curWord := t.wordMap[t.curWordKey]
+				curWord.typedKeyChan <- typedKey
 			} else {
 				// due to issue #3117, we gotta assign value like this for map of struct for now.
-				if tmp, exist := t.wordMap[key]; exist {
-					t.curWordKey = key
-					tmp.progress++
-					t.wordMap[key] = tmp
+				if curWord, exist := t.wordMap[typedKey]; exist {
+					t.curWordKey = typedKey
+					curWord.typedKeyChan <- typedKey
 				}
 			}
 		case <-time.After(500 * time.Millisecond):
@@ -160,7 +199,7 @@ func (t *TypingGeeks) GoEventTrigger() {
 				}
 			}
 			// TODO: this is blocking if you type fast, maybe use select? or use slice of channel?
-			t.keyChan <- event.Ch
+			t.typedKeyChan <- event.Ch
 		case termbox.EventResize:
 			fmt.Println("Resize")
 		case termbox.EventError:
